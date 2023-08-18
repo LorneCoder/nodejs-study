f248e26811aa5fac9a0cefa78296daab63f7c839
Lorne <627841904@qq.com>
2023-08-18 11:44:03 +0800

    预防xss攻击和sql注入

--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -1,4 +1,5 @@
 const { exec } = require('../db/mysql')
+const xss = require('xss')
 
 const getList = (author, keyword) => {
     let sql = `select * from blogs where 1=1 `
@@ -23,8 +24,8 @@ const getDetail = (id) => {
 
 const newBlog = (blogData = {}) => {
     // blogData 是一个博客对象，包含 title content author 属性
-    const title = blogData.title
-    const content = blogData.content
+    const title = xss(blogData.title)
+    const content = xss(blogData.content)
     const author = blogData.author
     const createTime = Date.now()
     const sql = `
--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -1,9 +1,13 @@
-const { exec } = require('../db/mysql')
+const { exec, escape } = require('../db/mysql')
 
 const login = (username, password) => {
+    username = escape(username) // 防止 sql 注入，使用 escape 来转义sql中的字符，可以防止 用户提交表单时，恶意注入 sql
+    password = escape(password)
+
     const sql = `
-        select username, realname from users where username='${username}' and password='${password}'
+        select username, realname from users where username=${username} and password=${password}
     `
+    // console.log('sql is ', sql)
     return exec(sql).then(rows => {
         return rows[0] || {}
     })
